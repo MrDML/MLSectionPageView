b3b43ad62189207850d890d05740e491098d1553
dml <dml1630@163.com>
2018-11-01 14:44:46 +0800

    initialize

---
+++ MLSectionPageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLSectionPageView
+//
+//  Created by 戴明亮 on 2018/11/1.
+//  Copyright © 2018年 ML Day. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLSectionPageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLSectionPageView
+//
+//  Created by 戴明亮 on 2018/11/1.
+//  Copyright © 2018年 ML Day. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
